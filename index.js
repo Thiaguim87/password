a4bb8c7bed8f59d9f7d0da3c06b41909308551bb
Thiaguim87 <tluiz87@outlook.com>
2023-09-18 08:37:18 -0300

    Removed unused variables

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 var page1 = document.getElementById('home');
-var page2 = document.getElementById('verify');
-var menu = document.getElementById('menu_button');
 var gen = document.getElementById('gen');
 var len = document.getElementById('length');
 var pass = document.getElementById('password');
 var copy = document.getElementById('copy');
 var visibilidade = false;
 
-menu.onclick= function() {
-    if (visibilidade) {
-        document.getElementById('menu_inner').style.display = "none";
-        visibilidade = false;
-        menu.innerHTML = "Ξ";
-    } else {
-        document.getElementById('menu_inner').style.display = "block";
-        visibilidade = true;
-        menu.innerHTML = "X";
-    }
-}
-
-page1.onclick = function() {
-    window.location.href = "./index.html";
-}
-page2.onclick = function() {
-    window.location.href = "./verify.html";
-}
 
 gen.onclick = function(){
     if(len.value != ''){
